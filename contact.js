366af5254e908ad870b6b723898968cd927c4fb3
Sjossa <johnny.sass2001@gmail.com>
2023-11-13 11:28:21 +0100

    changer input

--- contact.js
+++ contact.js
@@ -1,42 +1,81 @@
-// Bloquer l'entrée
-document.addEventListener("keydown", function(event) {
-  if (event.keyCode == 13) {
-      event.preventDefault(); // Empêche l'action par défaut de la touche "Entrée"
-      return false;
-  }
-}, true); // "true" => phase de capture
+document.addEventListener("DOMContentLoaded", function () {
+    // Utilisation d'une fonction nommée pour plus de clarté
+    document.getElementById("valider").addEventListener("click", changerInput);
 
-// Fonction pour passer automatiquement au champ suivant
-function suivant(jeu, suivant, limite) {
-  if (jeu.value.length == limite) {
-      var nextInput = jeu.nextElementSibling;
-      if (nextInput && nextInput.tagName.toLowerCase() === "input") {
-          nextInput.focus(); // Met le focus sur le champ suivant
-      }
-  }
+    document.addEventListener("keydown", function (event) {
+        // Utilisation de l'événement key === "Enter" plutôt que keyCode (qui est déprécié)
+        if (event.key === "Enter") {
+            event.preventDefault();
+            return false;
+        }
+    }, true);
+});
+
+function changeInput(input, suivant, limite) {
+    if (input.value.length === limite) {
+        var nextInput = document.getElementById(suivant);
+        // Utilisation de l'opérateur ternaire pour plus de concision
+        nextInput && nextInput.tagName.toLowerCase() === "input" && nextInput.focus();
+    }
 }
 
-//
-function suivant(input, targetId, maxLength) {
-  var inputValue = input.value;
-  var target = document.getElementById(targetId);
 
-  if (inputValue.length <= maxLength) {
-      // Ajoutez chaque lettre une par une avec un délai
-      for (var i = 0; i < inputValue.length; i++) {
-          setTimeout(function (index) {
-              return function () {
-                  target.innerHTML = inputValue.substring(0, index + 1);
+function changerInput() {
+    // Récupère l'élément d'entrée du prénom
+    var prenom = document.getElementById("prenom");
+    
+    // Crée un nouvel élément d'entrée pour le nom
+    var nom = document.createElement("input");
+    nom.type = "text";
+    nom.id = "nom";
+    nom.name = "game";
+    nom.maxlength = "10";
+    nom.size = "10";
+    nom.oniput = "changeInput(this, 'nom', 5)"
+    
+    // Remplace l'élément d'entrée du prénom par l'élément d'entrée du nom
+    prenom.parentNode.replaceChild(nom, prenom);
 
-                  // Désactiver le soulignement pour les caractères espacés
-                  if (inputValue[index] === ' ') {
-                      input.classList.add('no-underline');
-                  } else {
-                      input.classList.remove('no-underline');
-                  }
-              };
-          }(i), i * 500); // Ajustez la durée du délai (en millisecondes) selon vos besoins
-      }
-  }
+    var prenomL =document.querySelector('label[for="prenom"]');
+
+    prenomL.textContent = "veuillez entrer votre nom : ";
+    prenomL.setAttribute("for", "nom")
+
+    // Affiche le label et l'input pour le nom
+    document.getElementById("labelNom").style.display = "block";
+    nom.style.display = "block";
+
+    // Met le focus sur le nouvel élément d'entrée du nom
+    nom.focus();
 }
+
+function changerInput2(){
+
+    var nom = document.getElementById("nom");
+
+    var email = document.createElement("input");
+    email.type="email";
+    email.id ="email";
+    email.name = "game";
+    email.pattern = ".+@example\.com";
+    email.size  = "10";
+
+
+    nom.parentNode.replaceChild(email, nom);
+
+    var nomL = document.querySelector('label[for="nom"]');
+
+    nomL.textContent = " veuillez entrer votre adresse mail :"
+    nomL.setAttribute("for","email")
+
+    document.getElementById("labelemail").style.display = "block";
+    nom.style.display = " block"
+
+    email.focus();
+
+}
+
+
+
+  
 
