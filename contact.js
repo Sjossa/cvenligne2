aa357be98f7f8b8303a9bf8f63b2001f7feab03f
Sjossa <johnny.sass2001@gmail.com>
2023-11-13 16:43:01 +0100

    input

--- contact.js
+++ contact.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Utilisation d'une fonction nommée pour plus de clarté
     document.getElementById("valider").addEventListener("click", changerInput);
-
     document.addEventListener("keydown", function (event) {
-        // Utilisation de l'événement key === "Enter" plutôt que keyCode (qui est déprécié)
         if (event.key === "Enter") {
             event.preventDefault();
             return false;
@@ -14,65 +11,56 @@ document.addEventListener("DOMContentLoaded", function () {
 function changeInput(input, suivant, limite) {
     if (input.value.length === limite) {
         var nextInput = document.getElementById(suivant);
-        // Utilisation de l'opérateur ternaire pour plus de concision
         nextInput && nextInput.tagName.toLowerCase() === "input" && nextInput.focus();
     }
 }
 
-
 function changerInput() {
-    // Récupère l'élément d'entrée du prénom
     var prenom = document.getElementById("prenom");
     
-    // Crée un nouvel élément d'entrée pour le nom
     var nom = document.createElement("input");
     nom.type = "text";
     nom.id = "nom";
     nom.name = "game";
     nom.maxlength = "10";
     nom.size = "10";
-    nom.oniput = "changeInput(this, 'nom', 5)"
-    
-    // Remplace l'élément d'entrée du prénom par l'élément d'entrée du nom
+    nom.oninput = function () {
+        changeInput2(this, 'email', 5);
+    };
+
     prenom.parentNode.replaceChild(nom, prenom);
 
-    var prenomL =document.querySelector('label[for="prenom"]');
+    var prenomLabel = document.querySelector('label[for="prenom"]');
+    prenomLabel.textContent = "Veuillez entrer votre nom :";
+    prenomLabel.setAttribute("for", "nom");
 
-    prenomL.textContent = "veuillez entrer votre nom : ";
-    prenomL.setAttribute("for", "nom")
-
-    // Affiche le label et l'input pour le nom
     document.getElementById("labelNom").style.display = "block";
     nom.style.display = "block";
-
-    // Met le focus sur le nouvel élément d'entrée du nom
     nom.focus();
 }
 
-function changerInput2(){
-
+function changerInput2() {
     var nom = document.getElementById("nom");
 
     var email = document.createElement("input");
-    email.type="email";
-    email.id ="email";
+    email.type = "email";
+    email.id = "email";
     email.name = "game";
     email.pattern = ".+@example\.com";
-    email.size  = "10";
-
+    email.size = "10";
+    email.oninput = function () {
+        changeInput(this, 'suivant', 5);
+    };
 
     nom.parentNode.replaceChild(email, nom);
 
-    var nomL = document.querySelector('label[for="nom"]');
-
-    nomL.textContent = " veuillez entrer votre adresse mail :"
-    nomL.setAttribute("for","email")
+    var nomLabel = document.querySelector('label[for="nom"]');
+    nomLabel.textContent = "Veuillez entrer votre adresse mail :";
+    nomLabel.setAttribute("for", "email");
 
     document.getElementById("labelemail").style.display = "block";
-    nom.style.display = " block"
-
+    email.style.display = "block";
     email.focus();
-
 }
 
 
