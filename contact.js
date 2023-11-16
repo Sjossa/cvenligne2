e0ed3e2b01af96f4ece2ebd7c2ea288daf45bf4a
Sjossa <johnny.sass2001@gmail.com>
2023-11-16 16:36:17 +0100

    zero

--- contact.js
+++ contact.js
@@ -1,9 +1,5 @@
-// Attendez que le DOM soit entièrement chargé avant d'attacher des écouteurs d'événements
 document.addEventListener("DOMContentLoaded", function () {
-    // Attachez une fonction pour changer l'input lorsqu'on clique sur le bouton "valider"
     document.getElementById("valider").addEventListener("click", changerInput);
-
-    // Empêchez la soumission du formulaire lorsqu'on appuie sur la touche "Enter"
     document.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -11,106 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }, true);
 });
 
-var formulaireData = {};
-var etatFormulaire = "prenom";
-var coffre = {
-    prenom: "",
-    nom: "",
-    email: ""
-};
+function changeinput(){
 
-function envoyerFormulaire() {
-    var formulaire = document.forms["game"];
+    var bouton = document.getElementById(valider);
 
-    if (formulaire) {
-        // Utilisation directe des valeurs des champs
-        coffre.prenom = formulaire.prenom ? formulaire.prenom.value : "";
-        coffre.nom = formulaire.nom ? formulaire.nom.value : "";
-        coffre.email = formulaire.email ? formulaire.email.value : "";
+    bouton.addEventListener("click",function)
+    
+  
 
-        // Enregistrez les données dans la base de données ou effectuez d'autres opérations nécessaires ici
-        console.log("Données du formulaire prêtes à être envoyées :", coffre);
-    } else {
-        console.error("Le formulaire n'a pas été trouvé.");
-    }
 }
 
-function changerInput() {
-    var inputActuel;
-
-    switch (etatFormulaire) {
-        case "prenom":
-            inputActuel = document.getElementsByName("prenom")[0];
-            changerInputPrenom(inputActuel);
-            console.log("État du formulaire : prénom");
-            break;
-
-        case "nom":
-            inputActuel = document.getElementsByName("nom")[0];
-            changerInputGenerique(inputActuel, "email", "email", "votre e-mail");
-            console.log("État du formulaire : nom");
-            break;
-
-        case "email":
-            var emailInput = document.getElementsByName("email")[0];
-            if (validationEmailReussie(emailInput.value)) {
-                envoyerFormulaire();
-                etatFormulaire = "soumission";
-                console.log("État du formulaire : soumission");
-            } else {
-                console.log("Validation de l'e-mail échouée. Ne soumettez pas le formulaire.");
-            }
-            break;
-
-        case "soumission":
-            envoyerFormulaire(); // Appel de la fonction à la fin du processus du formulaire
-            console.log("État du formulaire : soumission");
-            break;
-    }
-}
-
-function changerInputPrenom(inputActuel) {
-    changerInputGenerique(inputActuel, "text", "nom", "votre nom");
-}
-
-function changerInputGenerique(inputActuel, type, inputSuivant, labelSuivant) {
-    if (!inputActuel) {
-        console.error("L'élément n'a pas été trouvé.");
-        return;
-    }
-
-    var valeurInputActuel = inputActuel.value.trim();
-
-    if (valeurInputActuel !== "") {
-        // Stocker la valeur dans l'objet coffre avant le remplacement
-        coffre[inputActuel.name] = valeurInputActuel;
-
-        var nouvelInput = document.createElement("input");
-        nouvelInput.type = type;
-        nouvelInput.id = inputSuivant;
-        nouvelInput.name = inputSuivant;
-        nouvelInput.maxLength = 25;
-        nouvelInput.size = 20;
-
-        var parent = inputActuel.parentNode; // Récupérer le parent de l'input actuel
-
-        // Remplacer l'input actuel par le nouvel input
-        inputActuel.replaceWith(nouvelInput);
-
-        var labelActuel = document.querySelector('label[for="' + inputActuel.id + '"]');
-        if (labelActuel) {
-            labelActuel.textContent = "Veuillez entrer " + labelSuivant + " :";
-            labelActuel.htmlFor = nouvelInput.id;
-        }
-
-        nouvelInput.style.display = "block";
-        nouvelInput.focus();
-
-        etatFormulaire = inputSuivant;
-    }
-}
-
-function validationEmailReussie(email) {
-    var pattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    return pattern.test(email);
-}
--- contact.php
+++ contact.php
@@ -7,42 +7,44 @@
 </head>
 <body>
 <?php
-    // Vérifie si le formulaire a été soumis
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Récupère les données du formulaire
-        $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : "";
-        $nom = isset($_POST["nom"]) ? $_POST["nom"] : "";
-        $email = isset($_POST["email"]) ? $_POST["email"] : "";
-
-        // Connexion à la base de données MySQL
-        $con = mysqli_connect("localhost", "root", "", "cv");
-
-        // Vérifier la connexion
-        if (!$con) {
-            die("Erreur : " . mysqli_connect_error());
-        }
-
-        // Échapper les données pour éviter les injections SQL
-        $prenom = mysqli_real_escape_string($con, $prenom);
-        $nom = mysqli_real_escape_string($con, $nom);
-        $email = mysqli_real_escape_string($con, $email);
-
-        // Requête SQL pour insérer les données dans la table appropriée
-        $query = "INSERT INTO utilisateur (prenom, nom, email) VALUES ('$prenom', '$nom', '$email')";
-
-        // Exécution de la requête
-        if (mysqli_query($con, $query)) {
-            echo "Enregistrement réussi dans la base de données.";
-        } else {
-            echo "Erreur lors de l'enregistrement : " . mysqli_error($con);
-        }
-
-        // Fermer la connexion à la base de données
-        mysqli_close($con);
-    }
-    var_dump($_POST);
-
+// Vérifie si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupère les données du formulaire
+    $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : "";
+    $nom = isset($_POST["nom"]) ? $_POST["nom"] : "";
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
     
+    // Connexion à la base de données MySQL
+    $con = mysqli_connect("localhost", "root", "", "cv");
+
+    // Vérifier la connexion
+    if (!$con) {
+        die("Erreur : " . mysqli_connect_error());
+    }
+
+    // Échapper les données pour éviter les injections SQL
+    $prenom = mysqli_real_escape_string($con, $prenom);
+    $nom = mysqli_real_escape_string($con, $nom);
+    $email = mysqli_real_escape_string($con, $email);
+
+    // Requête SQL préparée pour insérer les données dans la table appropriée
+    $query = "INSERT INTO utilisateur (prenom, nom, email) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($con, $query);
+
+    // Liage des paramètres
+    mysqli_stmt_bind_param($stmt, "sss", $prenom, $nom, $email);
+
+    // Exécution de la requête préparée
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Enregistrement réussi dans la base de données.";
+    } else {
+        echo "Erreur lors de l'enregistrement : " . mysqli_error($con);
+    }
+
+    // Fermer la connexion à la base de données
+    mysqli_close($con);
+}
 ?>
+
 </body>
 </html>
