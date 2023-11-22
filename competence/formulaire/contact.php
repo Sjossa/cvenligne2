9fbcc43f5bdb834141050d7a7065f141571dae36
Sjossa <johnny.sass2001@gmail.com>
2023-11-22 16:28:47 +0100

    responsive

--- competence/formulaire/contact.php
+++ competence/formulaire/contact.php
@@ -2,41 +2,39 @@
 <html lang="fr">
 
 <head>
+    <!-- Métadonnées du document -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
+
+    <!-- Liens vers les fichiers CSS et JavaScript -->
     <link rel="stylesheet" type="text/css" href="contact.css">
     <script src="contact.js" defer></script>
 
+    <!-- Lien vers la police de caractères Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    
     <!-- En-tête de la page -->
-    <div id="con">
+ 
         <h1>Contact</h1>
-    </div>
-    <!-- Tableau pour la BDD -->
-
-
-    <!-- Formulaire de contact -->
+            <!-- Formulaire de contact -->
     <div id="formulaire">
-        <div class="border-content"></div>
         <form action="contact-BDD.php" method="post" name="game" id="myForm">
             <fieldset>
                 <!-- Champ pour le prénom -->
-                <label id="labelprenom" style="display: block;">Merci de renseigner votre prénom :</label>
+                <label id="labelprenom" style="display: block;" size="200">Veuillez indiquer votre prénom :</label>
                 <input type="text" id="inputprenom" name="prenom" maxlength="25" size="20" style="display: block;" autocomplete="off" autofocus>
 
                 <!-- Champ pour le nom -->
-                <label id="labelnom" style="display: none;">Merci de renseigner votre nom :</label>
+                <label id="labelnom" style="display: none;">Veuillez indiquer votre nom :</label>
                 <input type="text" id="inputnom" name="nom" maxlength="25" size="20" style="display: none;" autocomplete="off" autofocus>
 
                 <!-- Champ pour l'adresse e-mail -->
-                <label id="labelemail" style="display: none;">Merci de renseigner votre adresse mail :</label>
+                <label id="labelemail" style="display: none;">Veuillez saisir votre adresse e-mail:</label>
                 <input type="email" id="inputemail" name="email" maxlength="25" size="20" style="display: none;" autocomplete="off" autofocus>
 
                 <!-- Bouton pour valider le formulaire -->
@@ -44,24 +42,58 @@
             </fieldset>
         </form>
     </div>
+   
+
+
+
+    <!-- Affichage du nombre de formulaires remplis et de visiteurs -->
     <?php 
-    $cnx = mysqli_connect("localhost", "root", "", "cv");
+        // Connexion à la base de données
+        $cnx = mysqli_connect("localhost", "root", "", "cv");
 
-    if(mysqli_connect_errno()) {
-        echo "erreur de connexion à la base ".mysqli_connexion_error(); 
-    }
-         $sql = "select id";
-    $result = mysqli_query($cnx, $sql);
-    echo "<table>";
-                    echo "<thead>";
-                    echo "<th>"."id"."</th>";
-                    echo "</thead>";
-                    echo "<tbody>";
+        // Vérification de la connexion
+        if(mysqli_connect_errno()) {
+            echo "Erreur de connexion à la base " . mysqli_connect_error(); 
+        }
 
+        // Requête pour la table "utilisateur"
+        $sqlUtilisateur = "SELECT id_utilisateur FROM utilisateur ORDER BY id_utilisateur DESC"; 
+        $resultUtilisateur = mysqli_query($cnx, $sqlUtilisateur);
 
+        // Requête pour la table "visiteur"
+        $sqlVisiteur = "SELECT id FROM visiteur ORDER BY id DESC"; 
+        $resultVisiteur = mysqli_query($cnx, $sqlVisiteur);
 
+        // Affichage du tableau avec les données
+        echo "<table>";
+        echo "<thead>";
+        echo "<tr>";
+        echo "<th></th>";
+        echo "<th></th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+
+        // Récupération et affichage de la dernière entrée de la table "utilisateur"
+        if ($rowUtilisateur = mysqli_fetch_assoc($resultUtilisateur)) {
+            echo "<tr>";
+            echo "<td>" ."Nombre de formulaires remplis :" . $rowUtilisateur['id_utilisateur'] . "</td>";
+            echo "</tr>";
+        }
+
+        // Récupération et affichage de la dernière entrée de la table "visiteur"
+        if ($rowVisiteur = mysqli_fetch_assoc($resultVisiteur)) {
+            echo "<tr>";
+            echo "<td>" ."Nombre de visiteurs :". $rowVisiteur['id'] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</tbody>";
+        echo "</table>";
+
+        // Fermeture de la connexion à la base de données
+        mysqli_close($cnx);
     ?>
-    
 </body>
 
 </html>
